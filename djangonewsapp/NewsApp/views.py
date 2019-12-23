d0e309e463dc7c1dda617644e9101d31e4f59a76
emailanuj <emai.anujdubey@gmail.com>
2019-12-23 17:52:48 +0530

    Using Context Django

--- djangonewsapp/NewsApp/views.py
+++ djangonewsapp/NewsApp/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 # Create your views here.
 def Home(request):
-    return render(request,'home.html')
+    context={
+        'Name':'Anuj Kumar Dubey',
+        'programming_languages':['PHP','Python','Java','C++','SQL']
+    }
+    return render(request,'home.html',context)
 
 def About(request):
     return render(request,'about.html')
