18f421cf1619abf982037080c087b8ef125d41d7
emailanuj <emai.anujdubey@gmail.com>
2019-12-28 12:54:38 +0530

    Multi Table Inheritance

--- djangonewsapp/NewsApp/admin.py
+++ djangonewsapp/NewsApp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import News,SportsNews
+from .models import News,SportsNews,Place,Restaurant
 # Register your models here.
 admin.site.register(News)
 admin.site.register(SportsNews)
+admin.site.register(Place)
+admin.site.register(Restaurant)
--- djangonewsapp/NewsApp/models.py
+++ djangonewsapp/NewsApp/models.py
@@ -17,4 +17,11 @@ class SportsNews(models.Model):
 
     def __str__(self):
         return self.author
+        
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=80)
 
+class Restaurant(Place):
+    serves_hot_dogs = models.BooleanField(default=False)
+    serves_pizza = models.BooleanField(default=False)
